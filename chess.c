b213e8a23654db95271f446db236befe9445e264
drenchedj <kozorog_a@mail.ru>
2018-05-01 19:09:42 +0600

    Errors correct

--- chess.c
+++ chess.c
@@ -1,34 +1,55 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-    int main() {
-    int i;
-    char a[9][9];
-    char gl[]={'r','n','b','q','k','b','n','r'};
-    char p='p';
-    memset(a, ' ', sizeof(a));
+int desk(char mass[9][9])
+{
+  int i,j;
+  for (i=0;i<9;i++)
+    {
+    for (j=0;j<9;j++)
+      {
+      printf ("  %c  ",mass[i][j]);
+      }
+    printf("\n\n");
+    }
+  return 0;
+ }
 
-    //цифры
-    for (i=0; i<8; i++) {
-    a[i][0]=56-i;
-    }
-    //буквы
-    for (i=1; i<9; i++){
-    a[8][i]=96 + i;
-    }
+int start(char mass[][9])
+{
+  for (int i=0;i<9;i++)
+    for (int j=0;j<9;j++)
+mass[i][j]=' ';
+mass[0][0]='X'; mass[1][0]='8';		mass[1][1]='R';	mass[2][1]='P';		mass[7][1]='p';	mass[8][1]='r';
+mass[0][1]='A';	mass[2][0]='7';		mass[1][2]='N';	mass[2][2]='P';		mass[7][2]='p';	mass[8][2]='n';
+mass[0][2]='B';	mass[3][0]='6';		mass[1][3]='B';	mass[2][3]='P';		mass[7][3]='p';	mass[8][3]='b';
+mass[0][3]='C';	mass[4][0]='5';		mass[1][4]='Q';	mass[2][4]='P';		mass[7][4]='p';	mass[8][4]='q';
+mass[0][4]='D';	mass[5][0]='4';		mass[1][5]='K';	mass[2][5]='P';		mass[7][5]='p';	mass[8][5]='k';
+mass[0][5]='E';	mass[6][0]='3';		mass[1][6]='B';	mass[2][6]='P';		mass[7][6]='p';	mass[8][6]='b';
+mass[0][6]='F';	mass[7][0]='2';		mass[1][7]='N';	mass[2][7]='P';		mass[7][7]='p';	mass[8][7]='n';	
+mass[0][7]='G';	mass[8][0]='1';		mass[1][8]='R';	mass[2][8]='P';		mass[7][8]='p';	mass[8][8]='r';
+mass[0][8]='H';
+ return printf("Format of move - 'a2-a4'\nMove realize only for 'P(p)'\n");
+}
 
-    for (i= 1;i<9; i++) {
-    a[0][i] = gl[i-1];
-    a[7][i] = gl[i-1];
-    a[1][i] = p;
-    a[6][i] = p-32;
-    }
-    
-    for (i=0;i<9;i++) {
-    for (int g=0;g<9; g++){
-    printf("%c", a[i][g]);
-    }
-    printf("\n");
-    }
-    return 0;
+
+
+int game(char mass[][9])
+{
+  //
+  return printf("\nGame over!\n");
+}
+
+int move()
+{
+  return 0;
+}
+
+int main()
+{
+  char mass[9][9];
+  start(mass);
+  desk(mass);
+  game(mass);
+  return 0;
 }
